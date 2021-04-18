c8d1d08fc1cc2852d5f93f55d290d07934db2a84
Lautaro Acuña <myspaceinilworld@gmail.com>
2021-04-18 00:09:40 -0300

    Agregar archivos de proyecto.

---
+++ Persona.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pyPersonaPeso
+{
+    class Persona
+    {
+        private String nombre;
+        private int edad;
+        private char sexo;
+        private double peso, altura;
+
+        public Persona(string nombre, int edad, double altura, char sexo, double peso) {
+            this.nombre = nombre;
+            this.edad = edad;
+            this.altura = altura;
+            this.sexo = sexo;
+            this.peso = peso;
+        }
+         
+        public string pNombre
+        {
+            set { nombre = value; }
+            get { return nombre; }
+        }
+
+        public int pEdad
+        {
+            set { edad = value; }
+            get { return edad; }
+        }
+
+        public char pSexo
+        {
+            set { sexo = value; }
+            get { return sexo; }
+        }
+
+        public double pPeso
+        {
+            set { peso = value; }
+            get { return peso; }
+        }
+        public double pAltura
+        {
+            set { altura = value; }
+            get { return altura; }
+        }
+
+
+
+        public int calcularIMC() {
+            double calc = peso / (altura * altura);
+            if (calc < 20){
+                return -1;
+            }else if (calc >= 20 & (calc) <= 25){
+                return 0;
+            }else {
+                return 1;
+            }
+        }
+
+        public bool esMayorDeEdad() {
+            if (edad >= 21)
+            {
+                return true;
+            }
+            else {
+                return false;
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pyPersonaPeso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Introduzca el nombre de la persona");
+            string nombre = Convert.ToString(Console.ReadLine());
+            System.Console.WriteLine("Introduzca la edad de la persona");
+            int edad = Convert.ToInt32(Console.ReadLine());
+            System.Console.WriteLine("Introduzca la altura de la persona");
+            double altura = Convert.ToDouble(Console.ReadLine());
+            System.Console.WriteLine("Introduzca el sexo de la persona F o M");
+            char sexo = Convert.ToChar(Console.ReadLine());
+            System.Console.WriteLine("Introduzca el peso de la persona");
+            double peso = Convert.ToDouble(Console.ReadLine());
+
+            Persona p1 = new Persona(nombre, edad, altura, sexo, peso);
+
+            int imc = p1.calcularIMC();
+
+            if (imc == -1)
+            {
+                System.Console.WriteLine("Bajo peso");
+            }
+            else if (imc == 0)
+            {
+                System.Console.WriteLine("Peso ideal");
+            }
+            else {
+                System.Console.WriteLine("Sobrepeso");
+            }
+
+            if (p1.pEdad >= 21) {
+                System.Console.WriteLine(p1.pNombre + " es mayor de edad");
+            }  else {
+                System.Console.WriteLine(p1.pNombre+" es menor de edad");
+            }
+            Console.ReadKey();
+        }
+    }
+}
